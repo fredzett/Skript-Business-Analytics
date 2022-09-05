c0fe93f92293abc64c4464ca366e3b316a5191eb
fredzett <felix.zeidler@gmail.com>
2022-09-05 21:54:27 +0200

    Änderungen Einführung Python

--- _build/jupyter_execute/chapters/chapter_03/Intro_Python.py
+++ _build/jupyter_execute/chapters/chapter_03/Intro_Python.py
@@ -42,13 +42,11 @@
 # 
 # `**` =  Potenzieren (❗ die Schreibweise für $3^4$ in Python lautet $3{**}4$)
 
-# Dieses Vorgehen ist jedoch meist nicht sinnvoll und wenig vorteilhaft gegenüber einem Taschenrechner. 
-# 
-# Als Analogie können wir uns kurz Excel (oder ein alternatives Tabellenkalkulationsprogramm) vorstellen. Dort schreiben wir auch typischerweise nicht in eine Zelle eine komplette Formel mit fest codierten Werten.
+# Dieses Vorgehen ist jedoch meist nicht sinnvoll und wenig vorteilhaft gegenüber z.B. einem Taschenrechner. Auch in einem typischen Tabellenkalkulationsprogramm wie Excel würden wir anders vorgehen, da wird dort typischerweise nicht eine komplette Formel mit fest codierten Werten in eine Zelle schreiben, da dies umständlich, wenig flexibel und fehleranfällig wäre. Gleiches gilt für Python.
 # 
 # ![Excel schlecht](../assets/Excel_bad.png)
 # 
-# Stattdessen würden sie die Werte auf verschiedene Zellen aufteilen und in einer Ergebniszelle verknüpfen. Dieses Vorgehen hat den Vorteil, dass die Annahmen für das Ergebnis viel transparenter sind. So wird z.B. deutlich, dass es sich bei der $12$, die in der Berechnung zwei Mal vorkommt um tatsächlich zwei verschiedene Annahmen handelt, die nur zufällig den selben Wert haben.  
+# In Excel würden wir stattdessen die Werte auf verschiedene Zellen aufteilen und in einer Ergebniszelle verknüpfen. Dieses Vorgehen hat den Vorteil, dass die Annahmen für das Ergebnis viel transparenter und Änderungen in Annahmen auf einen Blick ersichtlich sind. So wird z.B. deutlich, dass es sich bei der $12$, die in der Berechnung zwei Mal vorkommt (Zellen C5 und C7) um tatsächlich zwei verschiedene Annahmen handelt - sonst würden wir nicht zwei Inputparameter benötigen -, die nur zufällig den selben Wert haben.  
 # 
 # ![Excel gut](../assets/Excel_good.png)
 
@@ -65,7 +63,7 @@
 # variable = wert
 # ```
 # 
-# `variable` kann hier jeden Namen annehmen[^2]. Für `wert` können wir Ganzahlen, Dezimalzahlen, Wörter oder auch ganz andere Objekte - z.B. Graphen oder Dataframes die wir teilweise noch kennenlernen werden - einsetzen. 
+# `variable` kann hier jeden Namen annehmen[^2]. Für `wert` können wir Ganzahlen, Dezimalzahlen, Wörter oder auch ganz andere sog. Datentypen - z.B. Listen, Graphen oder Dataframes die wir jeweils noch kennenlernen werden - einsetzen. 
 # 
 # [^2]: Ausnahmen sind in Python bereits belegte Wörter, sog. Keywords, diese sind hier aufgeführt [keywords](https://www.w3schools.com/python/python_ref_keywords.asp).
 # 
@@ -89,7 +87,7 @@ satz = "Deutscher Meister wird nur der BVB"
 sehr_wichtige_variabel = 321
 
 
-# Diese Konvention ist für die Funktion eines Programms nicht entscheiden, jedoch erleichtert dies die Lesbarkeit des Codes.
+# Diese Konvention ist für die Funktion eines Programms nicht entscheiden, jedoch erleichtert diese die Lesbarkeit des Codes.
 
 # Wir können unser obiges Formelbeispiel also verbessern, in dem wir die Annahmen in verschiedene Variablen speichern und dann miteinander zu einem Ergebnis kombinieren. 
 
@@ -122,7 +120,7 @@ ergebnis
 # ```
 # ````
 # 
-# Wir suchen den Kapitalbarwert ($KW_0$) der Investition und können dies mit untenstehendem Code umsetzen.
+# Wir suchen den Kapitalbarwert ($KW_0$) der Investition und können diesen mit dem untenstehendem Code berechnen
 # 
 # 
 
@@ -138,24 +136,79 @@ KW = I_0 + CF_1/(1+i)**1 + CF_2/(1+i)**2 + CF_3/(1+i)**3
 KW
 
 
-# Die Umsetzung war einfach und hat viele Vorteile gegenüber einer Variante, in der wir keine Variablen nutzen. Jedoch hat der bis hierin vorgestellte Ansatz immer noch relativ viele Nachteile, z.B.:
+# Die Umsetzung war einfach und hat viele Vorteile gegenüber einer Variante, in der wir keine Variablen nutzen. Jedoch hat der bis hierhin vorgestellte Ansatz immer noch relativ viele Nachteile. 
 # 
-# 1. jeder Cashflow benötigt eine eigene Variable. Bei großen Zahlenreihen ist dies viel manueller Aufwand (und auch fehleranfällig)
+# Zum einen ist der Code sehr repetitive, d.h. wir müssen bspw. für jeden Cashflow eine eigene Variable definieren und addieren auch jeden Cashflow separat auf. Dies ist nicht nur ein relativ großer manueller Aufwand, sondern auch fehleranfällig. 
 # 
-# 2. die Formel für den Kapitalwert muss für jeden zusätzlichen Cashflow angepasst
+# Zum anderen muss die Formel für den Kapitalbarwert für jede unterschiedliche Anzahl an Cashflows angepasst werden. Hat ein Projekt bspw. zwei oder sechs verschiedene Cashflows, dann müsste die Formel angepasst werden. 
 # 
+# ```{admonition} Repetitiver Code
+# :class: tip
+# Als generelle Daumenregel können wir bereits an dieser Stelle darauf hinweisen, dass wir repetitiven Code generell vermeiden und wir diesen optimieren sollten. 
+# ```
 # 
-# Im Folgenden wollen wir versuchen unseren Code Schritt für Schritt zu verbessern bzw. zu optimieren. 
+# Im Folgenden wollen wir versuchen unseren Code  zu verbessern bzw. zu optimieren. Wir werden Schritt für Schritt neue Konzepte einführen. Der Code wird so weniger repetitv und flexibler einsetzbar. Die hier eingeführten Konzepte sind unabhängig vom gewählten Beispiel und generisch einsetzbar. 
 
 # ### Wichtige Regeln in Python
 
-# [to come]
+# An dieser Stelle macht es Sinn einige grundsätzliche Regeln von Python zu erläutern. 
 # 
-# 1. Einrücken / Leerzeichen haben eine Bedeutung
+# **Kommentare**: Statements, denen ein "`#`" vorangestellt wird, werden von Python nicht als Code erkannt. Wir können so Kommentare im Code einfügen.
+
+# In[70]:
+
+
+# Dies ist ein Statement, um einer Variable einen Wert zuzuweisen
+var = 3
+
+
+#  
+# **Einrücken zu Beginn eines Statments:** im Gegensatz nahezu allen anderen Programmiersprachen hat das Einrücken von Text eine Bedeutung. Ein Statement muss also zu Anfang einer Zeile beginnen. Es sei denn, es handelt sich um bestimmte Art von Statements, die wir noch kennenlernen werden (z.B. eine Wenn-Bedingung)
+
+# In[71]:
+
+
+# Code beginnt am Anfang einer Zeile
+var = 13
+
+
+# In[72]:
+
+
+# FEHLER: Code beginnt nicht zu Anfang einer Zeile
+   var = 13
+
+
+# Python gibt im obigen Beispiel eine Fehlermeldung aus. Diese signalisiert, dass der Python-Code nicht korrekt ist. Hierbei geht es nicht um inhaltliche Fehler, sondern um formale Fehler, d.h. Python kann das Statement nicht verstehen. Der Grund ist in diesem Falle ein `IndentationError`, d.h. ein Fehler wegen fehlerhaften Einrückens von Text. 
 # 
-# 2. Lange Zeilen kann ich unterbrechen mit `\` oder durch `( )`
-# 
-# 3. Hilfe durch ??
+# Hinweis: innerhalb eines Statements werden Leerzeichen hingegen ignoriert.
+
+# In[76]:
+
+
+var = 3         +               4
+var
+
+
+# **Umgang mit langen Statments:** lange Statements können mit "`()`" über mehrere Zeilen verbunden werden. Dies bietet sich auch für die Lesbarkeit von Code an. 
+
+# In[78]:
+
+
+komplexe_rechnung = (1234
+                     + 12
+                     - 13
+                     * 14
+                    )
+komplexe_rechnung
+
+
+# In[79]:
+
+
+komplexe_rechnung = 1234 + 12 - 13 * 14
+komplexe_rechnung
+
 
 # ### Datentypen
 
@@ -187,7 +240,7 @@ type(a)
 # 
 # Für das Programm bzw. den Computer, aber auch für uns als Datenanalyst oder Programmierer, sind Datentypen aus unterschiedlichen Gründen wichtig: 
 # 
-# **1. Speichplatz des Datentyps**  
+# **1. Speicherplatz des Datentyps**  
 # 
 # Jeder Datentyp benötigt unterschiedlich viel Speicher. So macht es für den Computer einen Unterschied, ob wir `42` oder `42.0` schreiben, da er für diese Variablen unterschiedlich viel Speicherplatz "freimacht". Im Rahmen unserer Beispiele ist der Unterschied nicht von Bedeutung. Jedoch kann die Nutzung von Datentypen, die weniger Speicherplatz benötigen sinnvoll sein, wenn wir mit großen Datenmengen umgehen wollen. Im Rahmen dieses Kurses ist das Speicherargument jedoch nicht von großer Bedeutung. 
 # 
@@ -237,11 +290,18 @@ ergebnis
 
 # #### Listen
 # 
-# Der Datentyp `list` (Liste) kann beliebig viele Variablen enhalten und wird mit "`[ ]`" erzeugt.
+# Der Datentyp `list` (Liste) kann beliebig viele Variablen enhalten und wird mit "`[ ]`" erzeugt. Auch wenn eine Liste theoretisch unterschiedliche Datentypen beinhalten kann, wird diese meist eingesetzt, um zusammenhängende Elemente zusammenzufassen (z.B. unsere Cashflows)
 # 
 # Beispiel:
 
-# In[40]:
+# In[67]:
+
+
+diverses = [1, 2, "a", "d", 4.5]
+diverses
+
+
+# In[68]:
 
 
 zahlen = [1,14, 3, 11]
@@ -269,7 +329,7 @@ zahl2
 # Der Index beginnt in Python bei allen Datentypen, die mehrere Elemente beinhalten, immer mit `0`. D.h. das erste Element befindet sich immer an Position `0` und NICHT an Position `1`. 
 # ```
 
-# Listen haben verschiedene Fähigkeiten. Viele dieser Fähigkeiten (in diesem Falle sprechen wir von Methoden) können sie aufrufen, in dem "`.`" drücken. 
+# Listen haben verschiedene Fähigkeiten. Viele dieser Fähigkeiten (die Fähigkeiten der Datentypen werden auch (Klassen-)`Methoden` genannt) können sie aufrufen, in dem "`.`" drücken. 
 # 
 # Beispiel: sortieren einer Liste erfolgt via "`.sort()`"
 
@@ -307,7 +367,18 @@ KW
 my_first_dict = {"Begrüßung": "Hallo", "liste_zahlen": [1,2,3,4], 2: 4.32}
 
 
-# In[51]:
+# Hinweis: wir können - zur besseren Lesbarkeit - das obige Beispiel auch wiefolgt schreiben. 
+
+# In[81]:
+
+
+my_first_dict = {"Begrüßung": "Hallo", 
+                 "liste_zahlen": [1,2,3,4], 
+                 2: 4.32}
+my_first_dict
+
+
+# In[82]:
 
 
 my_first_dict["Begrüßung"]
@@ -367,7 +438,7 @@ KW
 # 
 # 3. es gibt unterschiedliche Datentypen, die wir für unsere Analysen nutzen können und sollten. Nicht jeder Datentyp ist für jede Art der Problem geignet. Wir haben bisher `int`, `float`, `str`, `list` und `dict` kennengelernt. Bei diesen Datentypen handelt es sich um solche, die bereits standardmäßig in Python integriert sind (sog. `build-in types`[^4]). Wir werden im weiteren Verlauf noch sehr viel mächtigere Datentypen kennenlernen, die wir explizit für die Zwecke der Datenanalyse entwickelt wurden und sehr hilfreiche "Fähigkeiten" haben. 
 # 
-# Im folgenden Kapitel werden wir unser Beispiel weiter optimieren. Denn - so ehrlich müssen wir sein - unsere bisherige Optimierung lässt noch keinen großen Vorteil gegenüber einem Taschenrechner und insbesondere nicht gegenüber Excel erkennen.
+# Im folgenden Kapitel werden wir unser Beispiel weiter optimieren. Denn - so ehrlich müssen wir sein - unsere bisherige Optimierung lässt noch keinen großen Vorteil gegenüber einem Taschenrechner und insbesondere nicht gegenüber Excel erkennen. Auch wenn wir die repetitive Definition von Cashflow-Variablen vermieden haben, ist unsere Berechnung selber noch (i) repetitiv (wir schreiben die Variable `cash_flows` insgesamt 4x auf) und (ii) unflexibel, da unsere gesamte Berechnung weiterhin für jede andere Anzahl an Cashflows neu beschrieben werden müsste. 
 # 
 # [^4]: siehe [hier](https://docs.python.org/3/library/stdtypes.html) für weitere Informationen zu den `build-in types`. 
 
